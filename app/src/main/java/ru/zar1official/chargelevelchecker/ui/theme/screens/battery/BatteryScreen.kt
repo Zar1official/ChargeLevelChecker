8881123d55fc16021742459ce3f1d64c9700eb42
Zar <dimzar233445@gmail.com>
2022-03-05 01:30:58 +0300

    updates

--- app/src/main/java/ru/zar1official/chargelevelchecker/ui/theme/screens/battery/BatteryScreen.kt
+++ app/src/main/java/ru/zar1official/chargelevelchecker/ui/theme/screens/battery/BatteryScreen.kt
@@ -1,16 +1,20 @@
 package ru.zar1official.chargelevelchecker.ui.theme.battery
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.koin.androidx.compose.getViewModel
@@ -22,55 +26,69 @@ import ru.zar1official.chargelevelchecker.ui.theme.screens.components.ScreenTopA
 fun BatteryScreen() {
     val viewModel: BatteryViewModel = getViewModel()
     val chargeLevel = viewModel.charge.observeAsState(0)
+    val chargeVisible = viewModel.textVisibility.observeAsState(true)
+
     Scaffold(topBar = {
         ScreenTopAppBar(titleResource = R.string.home_title)
     }) {
-
-    }
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        if (chargeLevel.value != 0) {
-            Column {
-                Battery(width = 80, height = 400, chargeLevel = chargeLevel.value)
-                Spacer(modifier = Modifier.height(30.dp))
-                Box(modifier = Modifier.width(80.dp), contentAlignment = Alignment.Center) {
-                    Text(
-                        text = chargeLevel.value.toString() + "%",
-                        fontSize = 25.sp,
-                        fontWeight = FontWeight.Bold,
-                    )
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            if (chargeLevel.value != 0) {
+                Column {
+                    Battery(chargeLevel = chargeLevel.value, modifier = Modifier.clickable {
+                        viewModel.onShowChargeText()
+                    })
+                    if (chargeVisible.value) {
+                        Spacer(modifier = Modifier.height(30.dp))
+                        Box(modifier = Modifier.width(80.dp), contentAlignment = Alignment.Center) {
+                            Text(
+                                text = chargeLevel.value.toString() + "%",
+                                fontSize = 25.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                        }
+                    }
                 }
+            } else {
+                CircularProgressIndicator()
             }
-        } else {
-            CircularProgressIndicator()
         }
     }
 }
 
 @Composable
 fun Battery(
-    width: Int,
-    height: Int,
+    modifier: Modifier = Modifier,
+    width: Dp = 80.dp,
+    height: Dp = 150.dp,
     chargeLevel: Int
 ) {
+    val topBoxWidth = (width / 2.dp).dp
+    val topBoxHeight = height / 8
+    Box(
+        modifier = modifier
+            .padding(start = (width / 2 - topBoxHeight))
+            .size(width = topBoxWidth, height = height / 10)
+            .background(color = Color.DarkGray)
+    )
     Card(
-        shape = RoundedCornerShape(10.dp), modifier = Modifier
-            .width(width = width.dp)
-            .height(height = height.dp),
+        shape = RoundedCornerShape(10.dp), modifier = modifier
+            .width(width = width)
+            .height(height = height),
         elevation = 10.dp
     ) {
         Column {
-            val bottomHeight = (chargeLevel * height / 100)
+            val bottomHeight = (chargeLevel * height.value / 100).dp
             val topHeight = height - bottomHeight
             Box(
-                modifier = Modifier
+                modifier = modifier
                     .fillMaxWidth()
-                    .height(topHeight.dp)
+                    .height(topHeight)
                     .background(color = Color.Gray)
             )
             Box(
-                modifier = Modifier
+                modifier = modifier
                     .fillMaxWidth()
-                    .height(bottomHeight.dp)
+                    .height(bottomHeight)
                     .background(color = Color.Green)
             )
         }
--- app/src/main/java/ru/zar1official/chargelevelchecker/ui/theme/screens/battery/BatteryViewModel.kt
+++ app/src/main/java/ru/zar1official/chargelevelchecker/ui/theme/screens/battery/BatteryViewModel.kt
@@ -1,8 +1,19 @@
 package ru.zar1official.chargelevelchecker.ui.theme.screens.battery
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.zar1official.chargelevelchecker.data.storage.ChargeStore
 
-class BatteryViewModel(private val store: ChargeStore) : ViewModel() {
+class BatteryViewModel(store: ChargeStore) : ViewModel() {
     val charge = store.charge
+
+    private val _textVisibility = MutableLiveData<Boolean>().apply {
+        value = true
+    }
+    val textVisibility: LiveData<Boolean> = _textVisibility
+
+    fun onShowChargeText() {
+        _textVisibility.value = !_textVisibility.value!!
+    }
 }
